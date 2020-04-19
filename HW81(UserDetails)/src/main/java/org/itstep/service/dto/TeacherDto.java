f803599168949255128fbf8e73d2640669b4e367
lashkoEv <55919127+lashkoev@users.noreply.github.com>
2020-04-19 23:55:46 +0300

    HW 81 done

---
+++ HW81(UserDetails)/src/main/java/org/itstep/service/dto/TeacherDto.java
@@ -0,0 +1,47 @@
+package org.itstep.service.dto;
+
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class TeacherDto {
+    private Integer id;
+
+    @NonNull
+    @NotBlank
+    @Length(max = 50)
+    private String firstName;
+
+    @NonNull
+    @NotBlank
+    @Length(max = 50)
+    private String lastName;
+
+    @NotBlank
+    @NonNull
+    private String login;
+
+    @NotBlank
+    @NonNull
+    private String password;
+
+    @NotBlank
+    @NonNull
+    private String role;
+
+    private List<Integer> groupsId;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
